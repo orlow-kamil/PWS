43cf2c77dff916f2c9096ebd7bab386a1fa1f630
Kamil Orłow <orlow.kamil@gmail.com>
2021-01-04 12:06:28 +0100

    Upgrade system.
    -> remove Update
    -> create OnEnable and coroutine Patrolling
    -> create StartPosition

--- SystemPatrolling/Assets/Scripts/Enemy/PatrollingSystem.cs
+++ SystemPatrolling/Assets/Scripts/Enemy/PatrollingSystem.cs
@@ -9,7 +9,8 @@ namespace Enemy
     [RequireComponent(typeof(NavMeshAgent))]
     public class PatrollingSystem : MonoBehaviour
     {
-        [SerializeField] private List<PointPosition> pointList;
+        [SerializeField] private Transform startTransform = null;
+        [SerializeField] private List<PointPosition> pointList = new List<PointPosition>();
         [SerializeField] [Range(0.1f, 0.5f)] float remainingDistance = 0.25f;
 
         private int _pointIndex = 0;
@@ -24,25 +25,33 @@ namespace Enemy
             if (pointList.Count == 0)
                 throw new System.Exception("Path is empty");
         }
+        private void OnEnable()
+        {
+            if (startTransform is null)
+                this.transform.position = Vector3.zero;
+            else
+                this.transform.position = startTransform.position;
+
+            StartCoroutine(Patrolling());
+        }
         private void SetNewDestination() => _agent.destination = GetPointPosFromPath(_pointIndex);
 
         private void GoToNextPoint()
         {
-            SetNewDestination();
+            this.SetNewDestination();
             _pointIndex++;
             _pointIndex = _pointIndex <= pointList.Count - 1 ? _pointIndex : 0;
         }
 
-        private void Start()
+        private IEnumerator Patrolling()
         {
-            GoToNextPoint();
-        }
-
-
-        private void Update()
-        {
-            if (!_agent.pathPending && _agent.remainingDistance < remainingDistance)
-                GoToNextPoint();
+            _pointIndex = 0;    //always start from first point
+            while (this.gameObject.activeSelf)
+            {
+                if (!_agent.pathPending && _agent.remainingDistance < remainingDistance)
+                    this.GoToNextPoint();
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
