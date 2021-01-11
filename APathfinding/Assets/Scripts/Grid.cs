0c2f3afb15f4aa47f922550c827884601ca531fe
Kamil Orłow <orlow.kamil@gmail.com>
2021-01-11 21:48:29 +0100

    Grid map

---
+++ APathfinding/Assets/Scripts/Grid.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        ShowGrid();
+    }
+
+    public void ShowGrid()
+    {
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                debugTextArray[x, y] = UtilitsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f);
+                CreateGridLine(x, y);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    private void CreateGridLine(int x, int y)
+    {
+        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+    }
+
+    private Vector3 GetWorldPosition(int x, int y) => new Vector3(x, y) * cellSize + originPosition;
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            debugTextArray[x, y].text = gridArray[x, y].ToString();
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        GetXY(worldPosition, out int x, out int y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+            return -1;
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        GetXY(worldPosition, out int x, out int y);
+        return GetValue(x, y);
+    }
+}
---
+++ APathfinding/Assets/Scripts/Others/Testing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] Transform background;
+    private Grid grid;
+    private void Start()
+    {
+        grid = new Grid(4, 2, 10f, new Vector3(20, 0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilitsClass.GetMouseWorldPosition(background.position.z), 56);
+            //Debug.Log(UtilitsClass.GetMouseWorldPosition(background.position.z).ToString());
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Value: " + grid.GetValue(UtilitsClass.GetMouseWorldPosition(background.position.z)).ToString());
+        }
+    }
+}
