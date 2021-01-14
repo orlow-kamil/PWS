8b190e3518a16149686fe0bc52972f0ade8729a0
Kamil Orłow <orlow.kamil@gmail.com>
2021-01-14 21:59:35 +0100

    Project init

---
+++ DialogSystem/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float rotateSpeed = 10f;
+
+    private void Move()
+    {
+        float v = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(0, 0, v) * speed * Time.deltaTime;
+
+        this.transform.Translate(movement);
+    }
+
+    private void Rotate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(0, h, 0) * rotateSpeed * Time.deltaTime;
+
+        this.transform.Rotate(movement);
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+}
